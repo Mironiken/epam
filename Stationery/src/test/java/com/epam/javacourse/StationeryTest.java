309e1ee0f3ffa35823831096e1b6b78baafbf74c
Mironiken <mironike@yandex.ru>
2017-02-26 23:00:23 +0300

    Added tests to Stationery
    Signed-off-by: Mironiken <mironike@yandex.ru>

--- Stationery/src/test/java/com/epam/javacourse/StationeryTest.java
+++ Stationery/src/test/java/com/epam/javacourse/StationeryTest.java
@@ -1,7 +1,10 @@
 package com.epam.javacourse;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.time.Period;
 
 import static org.junit.Assert.*;
 
@@ -10,23 +13,32 @@ import static org.junit.Assert.*;
  */
 public class StationeryTest {
     @Test
-    public void testThatDifferentStationeryIsNotEquel(){
-
+    public void testThatDifferentStationeryIsNotEqual(){
+        Stationery that = new Stationery(10, Period.ofYears(1), StationaryName.Pen);
+        Stationery other = new Stationery(12, Period.ofYears(1), StationaryName.Pen);
+        assertFalse(that.equals(other));
     }
     @Test
     public void testThatEqualStationeriesIsEqual(){
-
+        Stationery that = new Stationery(10, Period.ofYears(1), StationaryName.Pen);
+        Stationery other = new Stationery(10, Period.ofYears(1), StationaryName.Pen);
+        assertTrue(that.equals(other));
     }
     @Test
     public void testThatEqualStationaryHaveEqualHashCode(){
-
+        Stationery that = new Stationery(10, Period.ofYears(1), StationaryName.Pen);
+        Stationery other = new Stationery(10, Period.ofYears(1), StationaryName.Pen);
+        assertTrue(that.hashCode() == other.hashCode());
     }
     @Test
     public void testThatDifferentStationaryHaveDifferentHashCode(){
-
+        Stationery that = new Stationery(10, Period.ofYears(1), StationaryName.Pen);
+        Stationery other = new Stationery(12, Period.ofYears(1), StationaryName.Pen);
+        assertFalse(that.hashCode() == other.hashCode());
     }
     @Test
     public void testThatOneStationaryHasEqualHashCode(){
-        
+        Stationery that = new Stationery(10, Period.ofYears(1), StationaryName.Pen);
+        assertTrue(that.hashCode() == that.hashCode());
     }
 }
