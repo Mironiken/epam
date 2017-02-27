0ac54c0f1bb48bccc78dda1f0d5f1491ef94a3a2
Mironiken <mironike@yandex.ru>
2017-02-27 19:15:17 +0300

    Added test method's names for StationaryList
    Signed-off-by: Mironiken <mironike@yandex.ru>

---
+++ Stationery/src/test/java/com/epam/javacourse/StationaryListTest.java
@@ -0,0 +1,34 @@
+package com.epam.javacourse;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created on 27.02.2017.
+ */
+public class StationaryListTest {
+
+    public void testThatWeCanCreateStationaryListFromOtherStationaryList(){
+
+    }
+
+    public void testThatWeCanCreateStationaryListFromOtherListOfStationary(){
+
+    }
+
+    public void testThatWeCanFindAddedElementInStationaryList(){
+
+    }
+
+    public void testThatWeCanAddElementInStationaryList(){
+
+    }
+
+    public void testThatWeCanDeleteAddedElementInStationaryList(){
+
+    }
+
+    public void testThatWeCanDeleteNotAddedElementInStatinaryList(){
+        
+    }
+
+}
